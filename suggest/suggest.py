44a40ee3ff2d0f48e4583ec895762a89942ab630
Hugo0911 <95459434+Hugo0911@users.noreply.github.com>
2023-02-07 23:40:28 -0500

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -78,6 +78,9 @@ class Suggest(commands.Cog):
                         },
                         upsert=True,
                     )
+                    embed=discord.Embed(title=Success, description=Your suggestion was posted!, color=self.bot.main_color)
+                            await ctx.send(embed=embed)
+                        )
                     re = config.get("reaction-emojis")
                     if re:
                         for r in re.get("emojis", []):
@@ -86,8 +89,6 @@ class Suggest(commands.Cog):
                             )
                             await asyncio.sleep(0.1)
                             await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
-                            embed=discord.Embed(title=Success, description=Your suggestion was posted!, color=self.bot.main_color)
-                            await ctx.send(embed=embed)
         else:
             await ctx.send(
                 embed=discord.Embed(
