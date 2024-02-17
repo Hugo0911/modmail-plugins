a4130c4c883cf1093c76c070e3a3067a89e0d1ee
Hugo0911 <95459434+Hugo0911@users.noreply.github.com>
2024-02-17 00:04:35 -0500

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -71,7 +71,7 @@ class Suggest(commands.Cog):
                         {"_id": "suggestions"},
                         {
                             "$set": {
-                                "next_id": next_id - 52,
+                                "next_id": next_id + 1,
                                 str(next_id): {"message_id": message.id,},
                             }
                         },
